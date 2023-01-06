85270b1f36c4b6ad70d3ecc7065c66e08d2117a8
RomjanHossain <romjanhossain726526@gmail.com>
2023-01-06 16:05:00 +0600

    change pin serializer

--- userapp/serializer.py
+++ userapp/serializer.py
@@ -1,4 +1,6 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (IntegerField, ModelSerializer,
+                                        Serializer, ValidationError)
+
 from userapp.models import NewUser
 
 
@@ -6,3 +8,9 @@ class NewUserSerializer(ModelSerializer):
     class Meta:
         model = NewUser
         fields = "__all__"
+
+
+# make a serializer for change pin which will take old pin and new pin (required)
+class ChangePinSerializer(Serializer):
+    old_pin = IntegerField(required=True)
+    new_pin = IntegerField(required=True)
