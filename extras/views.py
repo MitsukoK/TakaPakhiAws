d7f84dc3fca94277b291a682ac761ac64108d159
RomjanHossain <romjanhossain726526@gmail.com>
2023-01-06 11:23:12 +0600

    api view for offer and fund

--- extras/views.py
+++ extras/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+# from rest_framework import mixins
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from extras.models import FundModel, OfferModel
+from extras.serializations import FundSerializer, OfferSerializer
+
+
+class OfferListAPIView(ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = OfferModel.objects.all()
+    serializer_class = OfferSerializer
+
+
+class FundListAPIView(ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = FundSerializer
+
+    def get_queryset(self):
+        return FundModel.objects.filter(user=self.request.user)
